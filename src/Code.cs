df74b8b422d59f6084ca40cf3e3aa8a289a959b4
Pedro Reys <pedroreys@gmail.com>
2013-03-13 01:42:34 -0500

    First pass on the repl
    - Is able to parse simple expressions and local assignments.
    - Has rudimentary multiline support with token "\" at the end of
    multiline statements;
    - Lacks dynamic reference resolution (will fail if finds a using
      statement)

---
+++ src/Code.cs
@@ -0,0 +1,70 @@
+﻿namespace csx
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Roslyn.Compilers.CSharp;
+
+    public class Code
+    {
+        private List<string> _lines = new List<string>();
+
+        public string Text
+        {
+            get
+            {
+                var cleanLines = this._lines.Select(x => x.EndsWith("\\") ? x.Substring(0, x.Length - 1) : x);
+                return string.Join("\r\n", cleanLines);
+            }
+        }
+
+        public bool IsExit
+        {
+            get
+            {
+                if (this.IsEmpty()) return false;
+
+                var lastLine = this._lines.Last();
+
+                return lastLine == "exit()" || lastLine == "close()";
+            }
+        }
+
+        public bool IsMultiline
+        {
+            get
+            {
+                if (this.IsEmpty()) return false;
+
+                return this._lines.Last().EndsWith("\\");
+            }
+        }
+
+        public void AddLine(string lineText)
+        {
+            this._lines.Add(lineText);
+        }
+
+        public string GetLastLine()
+        {
+            return _lines.Last();
+        }
+
+        public void Clear()
+        {
+            this._lines.Clear();
+        }
+
+        public void ClearNonLocalDeclarationLines()
+        {
+            var localDeclarationLines = this._lines.Where(line => Syntax.ParseStatement(line) is LocalDeclarationStatementSyntax);
+            this.Clear();
+            _lines.AddRange(localDeclarationLines);
+        }
+
+        private bool IsEmpty()
+        {
+            return !this._lines.Any();
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace csx
+{
+    using Roslyn.Compilers;
+    using Roslyn.Scripting.CSharp;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var scriptEngine = new ScriptEngine();
+
+            scriptEngine.AddReference("System");
+
+            WritePromptLine();
+
+            var code = new Code();
+            var session = scriptEngine.CreateSession();
+            session.Execute("");
+            while (!code.IsExit)
+            {
+                try
+                {
+                    AddCodeLine(code);
+
+                    if (code.IsExit) break;
+                    
+                    var result = session.Execute(code.Text);
+                    
+                    Console.WriteLine(result);
+
+                    code.ClearNonLocalDeclarationLines();
+                }
+                catch (CompilationErrorException compilationErrorException)
+                {
+                    Console.WriteLine(compilationErrorException.Message);
+                    code.Clear();
+                }
+                finally
+                {
+                    WritePromptLine();
+                }
+            }
+
+            Console.WriteLine("bye");
+        }
+
+
+        private static void AddCodeLine(Code code)
+        {
+            code.AddLine(Console.ReadLine());
+
+            if (code.IsMultiline)
+                AddCodeLine(code);
+        }
+
+        private static void WritePromptLine()
+        {
+            Console.Write(">");
+        }
+    }
+}
